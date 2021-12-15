4ca495663cf0730704336cf02eb0d25be71f8f35
crasykote <77972085+crasykote@users.noreply.github.com>
2021-12-15 23:26:21 +0200

    hw11

---
+++ js/lesson11/homework11/script.js
@@ -0,0 +1,45 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+
+    // let f1 = document.forms.f1;
+    // f1.onsubmit = function (e) {
+    //     e.preventDefault();
+    //     let name = this.name.value;
+    //     let age = this.age.value;
+    //     console.log(name, age);
+    //     localStorage.setItem('userData', JSON.stringify({name, age}));
+    // };
+
+
+    // let inputs = document.getElementsByTagName('input');
+    // let name = inputs[0];
+    // let age = inputs[1];
+    // let btn = document.getElementsByTagName('button') [0];
+    // btn.onclick = function () {
+    //     let nameValue = name.value;
+    //     let ageValue = age.value;
+    //     localStorage.setItem('userData2', JSON.stringify({name: nameValue, age: ageValue}));
+    // };
+
+
+
+
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+    // let carForm = document.forms.carForm;
+    //
+    // carForm.onsubmit = function (e) {
+    //     e.preventDefault();
+    //     let model =this.model.value;
+    //     let type =this.type.value;
+    //     let volume =this.volume.value;
+    //     let car = {model: model, type: type, volume: volume};
+    //     let cars = JSON.parse(localStorage.getItem('cars')) || [];
+    //     console.log(cars);
+    //     cars.push(car);
+    //     localStorage.setItem('cars', JSON.stringify(cars));
+    //
+    // };
+
+
